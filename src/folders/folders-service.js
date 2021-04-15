277695ef9277867bedbc02511a783f69d51516af
Olivia Lewis <ollybaby12@gmail.com>
2021-04-15 14:33:13 -0400

    ts GET folders

--- src/folders/folders-service.js
+++ src/folders/folders-service.js
@@ -1,6 +1,7 @@
 const FoldersService = {
     getAllFolders(knex) {
         return knex.select('*').from('noteful_folders')
+        console.log('Get all folders was requested')
     },
 
     insertFolder(knex, newFolder) {
