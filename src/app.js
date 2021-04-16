1c664a98fcf99d8a856ffa216074986923ecaaab
Olivia Lewis <ollybaby12@gmail.com>
2021-04-15 22:49:36 -0400

    futher ts for deploy

--- src/app.js
+++ src/app.js
@@ -9,13 +9,15 @@ const notesRouter = require('./notes/notes-router')
 
 const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-    ? 'tiny'
-    : 'common';
+// const morganOption = (NODE_ENV === 'production')
+//     ? 'tiny'
+//     : 'common';
 
-app.use(morgan(morganOption))
-app.use(helmet())
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+    skip: () => NODE_ENV === 'test'
+}))
 app.use(cors())
+app.use(helmet())
 
 app.use('/api/notes', notesRouter)
 
